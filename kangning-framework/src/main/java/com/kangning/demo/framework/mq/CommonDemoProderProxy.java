89c82db35e5f02bb1bf295170967be9254ceee13
kangningj@tujia.com <kangningj@tujia.com>
2019-05-13 10:26:42 +0800

    RocketMq添加

--- kangning-framework/src/main/java/com/kangning/demo/framework/mq/CommonDemoProderProxy.java
+++ kangning-framework/src/main/java/com/kangning/demo/framework/mq/CommonDemoProderProxy.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author 加康宁 Date: 2019-04-22 Time: 15:50
+ * @author kangning Date: 2019-04-22 Time: 15:50
  * @version $Id$
  */
 public class CommonDemoProderProxy {
@@ -24,18 +24,40 @@ public class CommonDemoProderProxy {
 
     private String groupName;
 
-    public CommonDemoProderProxy(String nameSrv, String groupName) {
+    private String instanceName;
+
+    private int retryTimes;
+
+    public CommonDemoProderProxy(String nameSrv, String groupName, String instanceName, int retryTimes) {
         try {
             this.nameSrv = nameSrv;
             this.groupName = groupName;
+            this.instanceName = instanceName;
+            this.retryTimes = retryTimes;
+            //新建生产者对象
             this.defaultMQProducer = new DefaultMQProducer(groupName);
+            //设置nameserver
             this.defaultMQProducer.setNamesrvAddr(nameSrv);
+            //设置实例名称
+            this.defaultMQProducer.setInstanceName(instanceName);
+            //设置失败重试次数
+            this.defaultMQProducer.setRetryTimesWhenSendFailed(retryTimes);
+            //启动实例
             this.defaultMQProducer.start();
         } catch (MQClientException e) {
             logger.error("init rocket produce error", e);
         }
     }
 
+    /**
+     * 实际调用生产者发送消息的接口
+     * @param message
+     * @return
+     * @throws InterruptedException
+     * @throws RemotingException
+     * @throws MQClientException
+     * @throws MQBrokerException
+     */
     public SendResult sendMsg(Message message) throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
         return defaultMQProducer.send(message);
     }
@@ -63,6 +85,22 @@ public class CommonDemoProderProxy {
 
     public void setGroupName(String groupName) {
         this.groupName = groupName;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+
+    public int getRetryTimes() {
+        return retryTimes;
+    }
+
+    public void setRetryTimes(int retryTimes) {
+        this.retryTimes = retryTimes;
     }
 
     @Override
--- kangning-service/src/main/java/com/kangning/demo/rocketmq/producer/TestProducer.java
+++ kangning-service/src/main/java/com/kangning/demo/rocketmq/producer/TestProducer.java
@@ -4,7 +4,7 @@ import com.kangning.demo.framework.mq.BaseCommonProducer;
 import com.kangning.demo.model.vo.RegionInfo;
 
 /**
- * @author 加康宁 Date: 2019-04-22 Time: 16:57
+ * @author kangning Date: 2019-04-22 Time: 16:57
  * @version $Id$
  */
 public class TestProducer extends BaseCommonProducer<RegionInfo> {
